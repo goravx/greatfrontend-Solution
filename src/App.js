14f08ac36bffc6e61cddbbc50dcbfd3b8773365b
Gocoderunav <gangwargaurav240@gmail.com>
2025-07-07 19:07:28 +0530

    use clickanywherehook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import useClickAnywhere from './hooks/useClickAnywhere';
 
 function App() {
+const[count,setCount] = useState(0);
+
+useClickAnywhere(()=>{setCount((p)=>p+1)});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Click Count: {count}</h1>
+      <p>Click Anywhere on the page to increase the count</p>
     </div>
   );
 }
